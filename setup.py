23df19feea5508388ece5c12054ac121a6f1f388
hesstobi <me@tobiashess.de>
2019-04-26 15:56:43 +0200

    Update setup.py
    Add kombu with fixed version

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'celery==4.1.1',
+        'kombu==4.2.2',
         'boto3==1.4.7',
         'django-braces==1.11.0',
         'django-celery-beat==1.1.0',
